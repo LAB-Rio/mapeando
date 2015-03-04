c9cde83188ba25f28e449e964431d0a39217768c
Luiz Fonseca <eu@luiz.cc>
2015-03-04 14:24:17 -0300

    Try to fix factory error

--- app/assets/javascripts/angular/factories/category_factory.js
+++ app/assets/javascripts/angular/factories/category_factory.js
@@ -1,8 +1,8 @@
-demandsApp.factory("categoryFactory", function($resource) {
+demandsApp.factory("categoryFactory", ['$resource', function($resource) {
   return $resource("/categories/:id", { id: "@id", format: 'json' },
     {
       'index':   { method: 'GET', isArray: false },
       'show':    { method: 'GET', isArray: false }
     }
   );
-});
+}]);

b140ae2aa6c89eab89e803ad2e9ec8d9e1b1d29a
Luiz Fonseca <eu@luiz.cc>
2015-03-09 20:08:09 -0300

    Minor fix to mailer host definition

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.default_url_options = { :host => 'http://mapeando.rio.gov.br' }
+  
 end

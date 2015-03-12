9fca2b01c0e930bf24c1d02268f28a897fac536f
Luiz Fonseca <eu@luiz.cc>
2015-03-12 20:14:32 -0300

    Change initializer

--- config/initializers/rack_zippy.rb
+++ config/initializers/rack_zippy.rb
@@ -1,3 +1,3 @@
-if Rails.env.production? || Rails.env.staging?
+if Rails.env.production? or Rails.env.staging?
   Rails.application.config.middleware.swap(ActionDispatch::Static, Rack::Zippy::AssetServer)
 end

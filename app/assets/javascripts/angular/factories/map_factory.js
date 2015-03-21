d28a063ec027af9d3d31d0903a5e30f5f9c6fbc0
Luiz Fonseca <eu@luiz.cc>
2015-03-21 10:09:02 -0300

    Decrease level of the map zoom so users can see more pins.

--- app/assets/javascripts/angular/factories/map_factory.js
+++ app/assets/javascripts/angular/factories/map_factory.js
@@ -12,7 +12,7 @@ demandsApp.factory('mapFactory', ['$resource', function($resource){
         fullScreen: true, 
         scrollWheelZoom: true,
         maxBounds: bounds
-      }).setView([-22.9083, -43.2108], 13);
+      }).setView([-22.9083, -43.2108], 12);
 
       // 1 - Positron Dark: http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png 
       // 2 - MapBox: http://{s}.tiles.mapbox.com/v4/luizfonseca.7532f8a3/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibHVpemZvbnNlY2EiLCJhIjoiMTFNcXRXdyJ9.5PRw57nx5srpwP838-KjVQ

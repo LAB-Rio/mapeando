5adb5b0a20c4801a7af7133f8909c208d95e227e
Luiz Fonseca <eu@luiz.cc>
2015-03-22 16:30:11 -0300

    Make small fixes on the asset pipeline

--- config/initializers/non_digest_assets.rb
+++ config/initializers/non_digest_assets.rb
@@ -1,2 +1,4 @@
-NonStupidDigestAssets.whitelist += [/\*.html$/]
+#NonStupidDigestAssets.whitelist += [
+#  'index.html', 'signup.html', 'login.html', 'logout.html',
+#  'new.html','create.html', 'show.html', 'edit.html', 'about.html', 'confirmation.html']
 

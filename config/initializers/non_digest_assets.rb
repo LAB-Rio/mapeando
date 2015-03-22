85099746820d2aed884ac82f5db6a9184ea399cd
Luiz Fonseca <eu@luiz.cc>
2015-03-22 16:35:23 -0300

    Remove non digest initializer and final WIP for performance improvements on rendering JSON.

--- config/initializers/non_digest_assets.rb
+++
@@ -1,4 +0,0 @@
-#NonStupidDigestAssets.whitelist += [
-#  'index.html', 'signup.html', 'login.html', 'logout.html',
-#  'new.html','create.html', 'show.html', 'edit.html', 'about.html', 'confirmation.html']
-

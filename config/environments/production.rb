2e3258769e8de3aa34c380183a4ca3fba374901d
Frederick Silva <fredericksilva@outlook.com>
2016-06-13 17:44:15 +0000

    chage mail addons for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,11 +87,11 @@ Rails.application.configure do
 
 
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.mandrillapp.com',
+    :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['MANDRILL_USERNAME'],
-    :password       => ENV['MANDRILL_APIKEY'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
